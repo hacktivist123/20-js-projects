fffebddb8e56c9073730908f4fd7a80492532a52
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-04 06:35:28 +0100

    (ft:) display pause button when video is paused

--- HTML5-Video-Player/script.js
+++ HTML5-Video-Player/script.js
@@ -12,9 +12,10 @@ function toggleVideoStatus() {
 
 // update play/pause icon
 function updatePlayIcon() {
-  video.paused = play.innerHTML
-    ? '<i class="fa fa-play fa-2x/>'
-    : '<i class="fa fa-play fa-2x/>';
+  const iconChange = video.paused
+    ? (play.innerHTML = '<i class="fa fa-play fa-2x"><i/>')
+    : (play.innerHTML = '<i class="fa fa-pause fa-2x"><i/>');
+  return iconChange;
 }
 // update progress and timestamp
 function updateProgress() {
