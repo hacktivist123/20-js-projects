3724b7307f7c857c005ad0e3ec280cd4e6aecce3
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-04 06:20:00 +0100

    get play button to pause and play video

--- HTML5-Video-Player/script.js
+++ HTML5-Video-Player/script.js
@@ -4,14 +4,41 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+  return false;
+}
+// update progress and timestamp
+function updateProgress() {
+  return true;
+}
+
+// Set Video time
+function setVideoProgress() {
+  return true;
+}
+
+// Stop Video
+function stopVideo() {
+  return true;
+}
 // Event Listeners
-video.addEventListener('click', toggleVideoStatus());
-video.addEventListener('pause', updatePlayIcon());
-video.addEventListener('play', updatePlayIcon());
-video.addEventListener('timeupdate', updateProgress());
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
 
-play.addEventListener('click', toggleVideoStatus());
+play.addEventListener('click', toggleVideoStatus);
 
-stop.addEventListener('click', stopVideo());
+stop.addEventListener('click', stopVideo);
 
-progress.addEventListener('change', setVideoProgress());
+progress.addEventListener('change', setVideoProgress);
