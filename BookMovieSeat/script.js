26273dd2f82d207fe91342b489070261a6c540ef
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-04 04:33:44 +0100

    save movie and seat data to local storage

--- BookMovieSeat/script.js
+++ BookMovieSeat/script.js
@@ -5,27 +5,38 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = Number(movieSelect.value);
+
+// save selected movie index and price
+const setMovieData = (index, price) => {
+  localStorage.setItem('Movie Index', index);
+  localStorage.setItem('Movie Price', price);
+};
+
 // update total and count on select
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const selectedSeatsAmount = selectedSeats.length;
+
   /* copy selected seats into arr, 
   map through the arr & 
   return new array of indexes */
-  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-  console.log(seatsIndex);
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  // Store selected seats in localStorage
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
   count.innerText = selectedSeatsAmount;
   total.innerText = `N${selectedSeatsAmount * ticketPrice}`;
-  // copy selected seats into arr, map through the arr & returhn new array of indexes
 };
-// movie select event
 
-movieSelect.addEventListener('change', (e) => {
+// movie select event
+movieSelect.addEventListener('change', e => {
   ticketPrice = Number(e.target.value);
   updateSelectedCount();
+  setMovieData(e.target.selectedIndex, e.target.value);
 });
+
 // seats select event
-container.addEventListener('click', (e) => {
+container.addEventListener('click', e => {
   if (
     e.target.classList.contains('seat') &&
     !e.target.classList.contains('occupied')
