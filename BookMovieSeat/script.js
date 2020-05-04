6b93c03a0017206b9c3012e74ae49bebd29edce0
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-04 03:51:46 +0100

    added functionality for selecting seats and updating price

---
+++ BookMovieSeat/script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = Number(movieSelect.value);
+// update total and count on select
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsAmount = selectedSeats.length;
+  count.innerText = selectedSeatsAmount;
+  total.innerText = selectedSeatsAmount * ticketPrice;
+};
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+});
