a88bb5e7122f3c4ae53c9ed64edddac8c2fffd43
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-06 00:37:26 +0100

    create function structure

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -5,10 +5,17 @@ const showMillionairesButton = document.getElementById('show-millionaires');
 const sortButton = document.getElementById('sort');
 const totalWealthButton = document.getElementById('calculate-wealth');
 
+// Functions
+
+const addUser = () => {};
+const doubleMoney = () => {};
+const showMillioniares = () => {};
+const sort = () => {};
+const totalWealth = () => {};
+
 // Event Listners
 addUserButton.addEventListener('click', addUser);
 doubleMoneyButton.addEventListener('click', doubleMoney);
 showMillionairesButton.addEventListener('click', showMillioniares);
 sortButton.addEventListener('click', sort);
-totalWealthButton.addEventListener('click', totalWealth)
-
+totalWealthButton.addEventListener('click', totalWealth);
