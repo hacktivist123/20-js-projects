af3bb4438b98945f84b9ebae7e4579bd59f7e60a
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-05 22:47:39 +0100

    created eventlistners for DOM elements

--- Exchangify/script.js
+++ Exchangify/script.js
@@ -6,8 +6,15 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-//fetch exchcange rates and update DOM
-function calculate() {
+// fetch exchcange rates and update DOM
 
-}
+function calculate() {}
+
+// Event Listners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+// Call calculate function
 calculate();
