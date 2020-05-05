666728b5bbc79c3616b73818a5eb9f7a684fcf58
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-06 00:35:39 +0100

    (chore) pull in DOM elements and attack event listners to them

---
+++ DOM Array Methods/script.js
@@ -0,0 +1,14 @@
+// DOM Elements
+const addUserButton = document.getElementById('add-user');
+const doubleMoneyButton = document.getElementById('double');
+const showMillionairesButton = document.getElementById('show-millionaires');
+const sortButton = document.getElementById('sort');
+const totalWealthButton = document.getElementById('calculate-wealth');
+
+// Event Listners
+addUserButton.addEventListener('click', addUser);
+doubleMoneyButton.addEventListener('click', doubleMoney);
+showMillionairesButton.addEventListener('click', showMillioniares);
+sortButton.addEventListener('click', sort);
+totalWealthButton.addEventListener('click', totalWealth)
+
