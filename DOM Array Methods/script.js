c283e70f36cbc38b6329e56c206ca48458dee941
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-06 14:44:28 +0100

    style button on hover and click, format money value from api

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -1,9 +1,34 @@
 // DOM Elements
+const main = document.getElementById('main');
 const addUserButton = document.getElementById('add-user');
 const doubleMoneyButton = document.getElementById('double');
 const showMillionairesButton = document.getElementById('show-millionaires');
 const sortButton = document.getElementById('sort');
 const totalWealthButton = document.getElementById('calculate-wealth');
+
+// data from API
+
+let data = [];
+
+// fetch random user and add money
+const fetchRandomUser = async () => {
+  const res = await fetch('https://randomuser.me/api');
+  const fetchData = await res.json();
+  const user = fetchData.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: new Intl.NumberFormat('en-IN', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(Math.floor(Math.random() * 10000000)),
+  };
+  console.log(newUser);
+};
+
+fetchRandomUser();
+fetchRandomUser();
+fetchRandomUser();
+fetchRandomUser();
 
 // Functions
 
