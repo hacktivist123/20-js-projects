45fcbceaae96d1ad0dd55daac2040dc6d1e44051
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-07 03:11:45 +0100

    activate addUser button

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -10,40 +10,13 @@ const totalWealthButton = document.getElementById('calculate-wealth');
 const apiUrl = 'https://randomuser.me/api';
 const data = [];
 // fetch random user and add money
-const fetchRandomUser = async () => {
-  const res = await fetch(apiUrl);
-  console.log(res);
-  const fetchData = await res.json();
-  const user = fetchData.results[0];
-  const newUser = {
-    name: `${user.name.first} ${user.name.last}`,
-    money: new Intl.NumberFormat('en-IN', {
-      style: 'currency',
-      currency: 'USD',
-    }).format(Math.floor(Math.random() * 10000000)),
-  };
-  addUser(newUser);
-};
-
-fetchRandomUser();
-fetchRandomUser();
-fetchRandomUser();
-fetchRandomUser();
 
 // Functions
-const addUser = (obj) => {
-  data.push(obj);
-  updateDOM();
-};
-const doubleMoney = () => {};
-const showMillionaires = () => {};
-const sort = () => {};
-const totalWealth = () => {};
 const updateDOM = (providedData = data) => {
   // Clear Main Div
   main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
   // Loop through the data array
-  providedData.forEach(person => {
+  providedData.forEach((person) => {
     // Create a Div
     const element = document.createElement('div');
     // Add a class of person to our new element
@@ -55,8 +28,37 @@ const updateDOM = (providedData = data) => {
     main.appendChild(element);
   });
 };
+const addUserToDOM = (obj) => {
+  data.push(obj);
+  updateDOM();
+};
+const doubleMoney = () => {};
+
+const showMillionaires = () => {};
+
+const sort = () => {};
+
+const totalWealth = () => {};
+
+const fetchRandomUser = async () => {
+  const res = await fetch(apiUrl);
+  const fetchData = await res.json();
+  const user = fetchData.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: new Intl.NumberFormat('en-IN', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(Math.floor(Math.random() * 10000000)),
+  };
+  addUserToDOM(newUser);
+};
+
+fetchRandomUser();
+fetchRandomUser();
+fetchRandomUser();
 // Event Listeners
-addUserButton.addEventListener('click', addUser);
+addUserButton.addEventListener('click', fetchRandomUser);
 doubleMoneyButton.addEventListener('click', doubleMoney);
 showMillionairesButton.addEventListener('click', showMillionaires);
 sortButton.addEventListener('click', sort);
