40252f8060622900233a60d554369479928a5531
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-13 01:07:10 +0100

    display notification function and addeventlistener on keyboard key press

--- Hangman Game/script.js
+++ Hangman Game/script.js
@@ -20,7 +20,7 @@ const displayWord = () => {
   ${selectedWord
     .split('')
     .map(
-      (word) => `<span class="letter">
+      word => `<span class="letter">
     ${correctLetters.includes(word) ? word : ''}
   </span>
   `
@@ -33,5 +33,37 @@ const displayWord = () => {
     popup.style.display = 'flex';
   }
 };
+
+// Update the Wrong letters
+const updateWrongLettersEl = () => {};
+
+// Show Notification
+const showNotification = () => {
+  notification.classList.add('show');
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+};
+// Keyboard Letter Press Event Handler
+window.addEventListener('keydown', e => {
+  // console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else if (!wrongLetters.includes(letter)) {
+      wrongLetters.push(letter);
+      updateWrongLettersEl();
+    } else {
+      showNotification();
+    }
+  }
+});
 
 displayWord();
